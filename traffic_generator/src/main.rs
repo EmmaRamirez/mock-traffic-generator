bafa39111d4104a100f3ad30ee2b1297d36b3315
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-02-28 15:56:41 -0700

    add ability to send random bit of data w/ json

--- traffic_generator/src/main.rs
+++ traffic_generator/src/main.rs
@@ -1,10 +1,11 @@
 #[macro_use] extern crate nickel;
 extern crate rustc_serialize;
+extern crate rand;
 
-use rustc_serialize::json;
-use std::io::Read;
-use std::sync::{Arc, Mutex};
+use rustc_serialize::json::{Json, ToJson};
+use rand::Rng;
 use std::collections::BTreeMap;
+use nickel::status::StatusCode;
 use nickel::{Nickel, JsonBody, HttpRouter, MediaType};
 
 #[derive(RustcDecodable, RustcEncodable)]
@@ -15,21 +16,29 @@ struct TrafficData {
 impl ToJson for TrafficData {
     fn to_json(&self) -> Json {
         let mut map = BTreeMap::new();
+        map.insert("data".to_string(), self.data.to_json());
+        Json::Object(map)
     }
-}
-
-fn generator() -> &'static str {
-    "It works."
 }
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.utilize(router! {
-        get "/" => |_req, _res| {
-            generator()
-        }
+    server.post("/", middleware! { |request, response|
+        let traffic_data = try_with!(response, {
+            request.json_as::<TrafficData>().map_err(|e| (StatusCode::BadRequest, e))
+        });
+        format!("{}", traffic_data.data)
     });
 
-    server.listen("127.0.0.1:6767");
+    server.get("/traffic", middleware! { |_, mut res|
+        let mut rng = rand::thread_rng();
+        let traffic_data = TrafficData {
+            data: rng.gen::<i32>()
+        };
+        res.set(MediaType::Json);
+        r#"{ "data": "traffic_data" }"#
+    });
+
+    server.listen("127.0.0.1:6767").unwrap();
 }
