fe6a8d1f167ca766657efe14fbcb7ae989b93202
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-04 20:01:18 -0700

    WIP add support for sockets.
    This commit adds WebSockets (standard, not socket.io)

--- traffic_generator/src/main.rs
+++ traffic_generator/src/main.rs
@@ -2,6 +2,7 @@
 extern crate hyper;
 extern crate rustc_serialize;
 extern crate rand;
+extern crate websocket;
 
 use rustc_serialize::json::{Json, ToJson};
 use rand::Rng;
@@ -9,6 +10,9 @@ use std::collections::BTreeMap;
 // use hyper::header::{AccessControlAllowOrigin, AccessControlAllowHeaders};
 use nickel::status::StatusCode;
 use nickel::{Nickel, StaticFilesHandler, JsonBody, HttpRouter};
+use websocket::{Server, Message, Sender, Reciever};
+use websocket::message::Type;
+use websocket::header::WebSocketProtocol;
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct TrafficData {
@@ -43,6 +47,55 @@ fn main() {
         };
         traffic_data.to_json()
     });
+
+    for connection in server {
+        thread::spawn(move || {
+            let request = connection.unwrap().read_request().unwrap();
+            let headers = request.headers.clone();
+
+            request.validate().unwrap();
+
+            let mut response = request.accept();
+
+            if let Some(&WebSocketProtocol(ref protocols)) = headers.get() {
+                if protocols.contains(&("traffic".to_string())) {
+                    response.headers.set(WebSocketProtocol(vec!["traffic".to_string()]));
+                }
+            }
+
+            let mut client = response.send().unwrap();
+
+            let ip = client.get_mut_sender()
+                        .get_mut()
+                        .peer_addr()
+                        .unwrap();
+
+            println!("Connection from {}", ip);
+
+            let message: Message = Message::text("Hello".to_string());
+            client.send_message(&message).unwrap();
+
+            let (mut sender, mut reciever) = client.split();
+
+            for message in reciever.incoming_messages() {
+                let message: Message = message.unwrap();
+
+                match message.opcode {
+                    Type::Close => {
+                        let message = Message::close();
+                        sender.send_message(&message).unwrap();
+                        println!("Client {} disconnected", ip);
+                        return;
+                    },
+                    Type::Ping => {
+                        let message = Message::pong(message.payload);
+                        sender.send_message(&message).unwrap();
+                    }
+                    _ => sender.send_message(&message).unwrap(),
+                }
+            }
+        })
+    }
 
     server.listen("127.0.0.1:6767").unwrap();
 }
